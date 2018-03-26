8150c049d4f196d9c609c327451ff45bba31a755
bjwangfei3 <bjwangfei3@corp.netease.com>
2018-03-26 11:00:08 +0800

    init commit

---
+++ drsp-eureka-server/src/main/java/com/github/happut/drspeurekaserver/DrspEurekaServerApplication.java
@@ -0,0 +1,12 @@
+package com.github.happut.drspeurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrspEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrspEurekaServerApplication.class, args);
+	}
+}
